33247945d2a960e3caef9e2ad180fa4804e782b6
AmrKhaled <yamr3212@gmail.com>
2024-11-06 21:49:19 +0200

    adding the communication from the container into the sub-apps

--- mfp/packages/marketing/src/bootstrap.js
+++ mfp/packages/marketing/src/bootstrap.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './App';
+import { createMemoryHistory } from 'history';
 
-const mount = (el) => {
-	ReactDom.render(<App />, el);
+const mount = (el, { onNavigate }) => {
+	const history = createMemoryHistory();
+	if (onNavigate) {
+		history.listen(onNavigate);
+	}
+	ReactDom.render(<App history={history} />, el);
 };
 
 if (process.env.NODE_ENV === 'development') {
 	const el = document.querySelector('#_marketing-dev-root');
 	if (el) {
-		mount(el);
+		mount(el, {});
 	}
 }
 
