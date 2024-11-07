985a959f8ea382a6613697b147e96618051176ed
AmrKhaled <yamr3212@gmail.com>
2024-11-07 19:24:29 +0200

    adding the signin and signup screen and fixing the navigation issue

---
+++ mfp/packages/auth/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Switch, Router, Route } from 'react-router-dom';
+import { StylesProvider } from '@material-ui/core/styles';
+import Signin from './components/Signin';
+import Signup from './components/Signup';
+
+export default ({ history }) => {
+    return (
+        <StylesProvider>
+            <Router history={history}>
+                <Switch>
+                    <Route path="/auth/signin" component={Signin} />
+                    <Route path="/auth/signup" component={Signup} />
+                </Switch>
+            </Router>
+        </StylesProvider>
+    );
+};
--- mfp/packages/container/src/App.js
+++ mfp/packages/container/src/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import MarketingApp from './components/MarketingApp';
+import React, {lazy, Suspense } from 'react';
 import Header from './components/Header';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+const MarketingLazy = lazy(() => import('./components/MarketingApp'));
+const AuthLazy = lazy(() => import('./components/AuthApp'));
 
 export default () => {
     return (
         <BrowserRouter>
             <Header />
-            <MarketingApp />
+            <Suspense fallback={<div>Loading...</div>}>
+                <Switch>
+                    <Route path="/auth" component={AuthLazy} />
+                    <Route path="/" component={MarketingLazy} />
+                </Switch>
+            </Suspense>
         </BrowserRouter>
     );
 };
