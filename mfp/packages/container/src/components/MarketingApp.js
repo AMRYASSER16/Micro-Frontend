cf933ab4fa8fcb349dd92f3d06c0f2d19e0644c5
AmrKhaled <yamr3212@gmail.com>
2024-11-07 18:08:13 +0200

    fixing the route issue

--- mfp/packages/container/src/components/MarketingApp.js
+++ mfp/packages/container/src/components/MarketingApp.js
@@ -7,13 +7,16 @@ export default () => {
     const history = useHistory();
 
     useEffect(() => {
-        mount(ref.current, {
+        const { onParentNavigate } = mount(ref.current, {
             onNavigate: ({ pathname: nextPathname }) => {
-                if (history.location.pathname !== nextPathname) {
+                const { pathname } = history.location;
+                if (pathname !== nextPathname) {
                     history.push(nextPathname);
                 }
             }
         });
+
+        history.listen(onParentNavigate);
     }, []);
 
     return <div ref={ref} />;
--- mfp/packages/marketing/src/bootstrap.js
+++ mfp/packages/marketing/src/bootstrap.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './App';
-import { createMemoryHistory } from 'history';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 
-const mount = (el, { onNavigate }) => {
-	const history = createMemoryHistory();
+const mount = (el, { onNavigate, defaultHistory }) => {
+	const history = defaultHistory || createMemoryHistory();
 	if (onNavigate) {
 		history.listen(onNavigate);
 	}
 	ReactDom.render(<App history={history} />, el);
+
+	return {
+		onParentNavigate({ pathname: nextPathname }) {
+			const { pathname } = history.location;
+			if (pathname !== nextPathname) {
+				history.push(nextPathname);
+			}
+		}
+	}
 };
 
 if (process.env.NODE_ENV === 'development') {
 	const el = document.querySelector('#_marketing-dev-root');
 	if (el) {
-		mount(el, {});
+		mount(el, { defaultHistory: createBrowserHistory() });
 	}
 }
 
